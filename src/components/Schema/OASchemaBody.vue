1ec0a59ac3f410370a00783a47fbdaab8a335739
enzonotario <enzo.notario@gmail.com>
2024-08-28 20:12:38 -0300

    feat: Schema components

---
+++ src/components/Schema/OASchemaBody.vue
@@ -0,0 +1,36 @@
+<script setup>
+const props = defineProps({
+  schema: {
+    type: Object,
+    required: true,
+  },
+  responseType: {
+    type: String,
+    required: true,
+  },
+})
+</script>
+
+<template>
+  <div class="flex flex-col">
+    <span
+      v-if="props.responseType === 'array'"
+      class="font-bold text-gray-700 dark:text-gray-300"
+    >
+      array of:
+    </span>
+
+    <div
+      v-if="props.schema"
+      class="flex flex-col pl-3 border-l-2 border-gray-200 dark:border-gray-800 border-l-solid"
+    >
+      <OASchemaProperty
+        v-for="(property, name) in props.schema.properties"
+        :key="name"
+        :property="property"
+        :name="name"
+        :schema="props.schema"
+      />
+    </div>
+  </div>
+</template>
---
+++ src/components/Schema/OASchemaProperty.vue
@@ -0,0 +1,36 @@
+<script setup>
+import OASchemaBody from "./OASchemaBody.vue";
+
+const props = defineProps({
+  property: {
+    type: Object,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  schema: {
+    type: Object,
+    required: true,
+  },
+})
+</script>
+
+<template>
+  <div class="flex flex-col">
+    <div class="flex flex-row space-x-4 text-sm">
+      <span class="font-bold">{{ props.name }}</span>
+      <span class="text-gray-600 dark:text-gray-400">{{ props.property?.type }}</span>
+      <span class="flex-grow" />
+      <span class="text-red-800 dark:text-red-200">{{ props.schema.required && props.schema.required.includes(name) ? 'required' : '' }}</span>
+    </div>
+
+    <OASchemaBody
+      v-if="props.property?.type === 'array'"
+      :schema="props.property"
+      :response-type="props.property.type"
+      class="pl-4"
+    />
+  </div>
+</template>
