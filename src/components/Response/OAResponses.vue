9e7464dc034921985ad1870b46df4008e3251132
enzonotario <enzo.notario@gmail.com>
2024-08-31 19:53:11 -0300

    change(OAResponses) ResponseCode tabs design

--- src/components/Response/OAResponses.vue
+++ src/components/Response/OAResponses.vue
@@ -21,17 +21,18 @@ const responsesCodes = Object.keys(props.responses)
 </script>
 
 <template>
-  <div class="flex flex-col">
+  <div class="flex flex-col -mt-[52px]">
     <Tabs
       :default-value="responsesCodes && responsesCodes[0]"
     >
-      <TabsList class="w-full relative">
-        <TabsIndicator class="absolute left-0 h-[2px] bottom-0 w-[--radix-tabs-indicator-size] translate-x-[--radix-tabs-indicator-position] rounded-full transition-[width,transform] duration-300 bg-black dark:bg-white" />
+      <TabsList class="relative w-full bg-transparent">
+        <TabsIndicator class="absolute left-0 h-full bottom-0 w-[--radix-tabs-indicator-size] translate-x-[--radix-tabs-indicator-position] rounded transition-[width,transform] duration-300 bg-muted" />
+        <span class="flex-1" />
         <TabsTrigger
           v-for="responseCode in responsesCodes"
           :key="responseCode"
           :value="responseCode"
-          class="h-full"
+          class="h-full z-10"
         >
           {{ responseCode }}
         </TabsTrigger>
