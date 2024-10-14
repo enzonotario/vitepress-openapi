6a203f8100fe925f073a6db1cd0f8914500fe426
enzonotario <enzo.notario@gmail.com>
2024-10-14 19:10:43 -0300

    chore(docs theme): remove customization

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -10,21 +10,6 @@ export default {
     const openapi = useOpenapi()
     openapi.setSpec(spec)
 
-    // Optionally, set the i18n configuration.
-    const themeConfig = useTheme()
-    themeConfig.setI18nConfig({
-      messages: {
-        en: {
-          ...locales.en,
-          'operation.badgePrefix.operationId': 'Operation ID: ',
-        },
-        es: {
-          ...locales.es,
-          'operation.badgePrefix.operationId': 'ID de operación: ',
-        },
-      },
-    })
-
     // Use the theme.
     theme.enhanceApp({ app })
   },
