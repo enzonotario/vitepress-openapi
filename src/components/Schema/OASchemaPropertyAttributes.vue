1ea85b552b47f40cd9c51ad7e686594ae0c6adec
enzonotario <enzo.notario@gmail.com>
2024-10-16 20:01:45 -0300

    feat(OAParameter): show example/s value

--- src/components/Schema/OASchemaPropertyAttributes.vue
+++ src/components/Schema/OASchemaPropertyAttributes.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { titleCase } from 'scule'
+import OACodeValue from 'vitepress-openapi/components/Common/OACodeValue.vue'
 
 const props = defineProps({
   property: {
@@ -9,11 +10,13 @@ const props = defineProps({
 })
 
 const keysToIgnore = ['type', 'description', 'properties', 'required', 'items', 'xml', 'allOf', 'anyOf', 'oneOf', 'not']
+
+const properties = Object.keys(props.property).filter(key => !keysToIgnore.includes(key))
 </script>
 
 <template>
   <div
-    v-for="(key, propertyIdx) in Object.keys(props.property).filter(k => !keysToIgnore.includes(k))"
+    v-for="(key, propertyIdx) in properties"
     :key="propertyIdx"
     class="flex flex-row flex-wrap items-center gap-2"
   >
@@ -21,19 +24,6 @@ const keysToIgnore = ['type', 'description', 'properties', 'required', 'items', 
       {{ titleCase(key) }}
     </span>
 
-    <template v-if="Array.isArray(props.property[key])">
-      <code
-        v-for="(value, attributeIdx) in props.property[key]"
-        :key="attributeIdx"
-        class="!text-xs text-wrap break-all"
-      >
-        {{ value }}
-      </code>
-    </template>
-    <code
-      v-else class="!text-xs"
-    >
-      {{ props.property[key] }}
-    </code>
+    <OACodeValue :value="props.property[key]" />
   </div>
 </template>
