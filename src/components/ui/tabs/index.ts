77184be2eff21723efdcc2c72333b30bbc6d421f
enzonotario <enzo.notario@gmail.com>
2024-10-30 20:43:36 -0300

    chore(tabTrigger): unified styles

--- src/components/ui/tabs/index.ts
+++ src/components/ui/tabs/index.ts
@@ -6,19 +6,16 @@ export { default as TabsList } from './TabsList.vue'
 export { default as TabsContent } from './TabsContent.vue'
 
 export const tabsTriggerVariants = cva(
-  '',
+  'inline-flex items-center justify-center whitespace-nowrap rounded-sm px-3 py-1.5 text-sm font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50',
   {
     variants: {
       variant: {
         default: 'inline-flex items-center justify-center whitespace-nowrap rounded-sm px-3 py-1.5 text-sm font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:text-foreground',
+        default: 'data-[state=active]:text-foreground',
         select: 'relative flex w-full',
-        schemaTabs: 'inline-flex items-center justify-center whitespace-nowrap rounded-sm px-3 py-1.5 text-sm font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-background data-[state=active]:text-foreground data-[state=active]:shadow-sm',
+        schemaTabs: 'data-[state=active]:bg-background data-[state=active]:text-foreground data-[state=active]:shadow-sm',
       },
-    },
-    defaultVariants: {
-      variant: 'default',
     },
   },
 )
-
 export type TabsTriggerVariants = VariantProps<typeof tabsTriggerVariants>
