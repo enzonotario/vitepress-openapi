54954c94d7cb3c7398206eb8e9ba4b79b056fb6d
enzonotario <enzo.notario@gmail.com>
2024-11-01 19:57:33 -0300

    chore: const

--- src/lib/generateMissingTags.ts
+++ src/lib/generateMissingTags.ts
@@ -7,10 +7,12 @@ export function generateMissingTags(spec: any) {
     return spec
   }
 
+  const defaultTag = useTheme().getSpecConfig().defaultTag
+
   for (const path of Object.values(paths)) {
     for (const verb of Object.keys(path)) {
       const operation = path[verb]
-      const tags = operation.tags || [useTheme().getSpecConfig().defaultTag]
+      const tags = operation.tags || [defaultTag]
       operation.tags = tags
     }
   }
