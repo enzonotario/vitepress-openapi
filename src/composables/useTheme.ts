25b3e577d5c05a367dea49e55b9c7c9d7bc1eb56
enzonotario <enzo.notario@gmail.com>
2024-11-01 20:00:03 -0300

    chore: types

--- src/composables/useTheme.ts
+++ src/composables/useTheme.ts
@@ -73,12 +73,12 @@ export interface I18nConfig {
 }
 
 export interface SpecConfig {
-  groupByTags: Ref<boolean>
-  collapsePaths: Ref<boolean>
-  showPathsSummary: Ref<boolean>
-  avoidCirculars: Ref<boolean>
-  lazyRendering: Ref<boolean>
-  defaultTag: string
+  groupByTags?: Ref<boolean>
+  collapsePaths?: Ref<boolean>
+  showPathsSummary?: Ref<boolean>
+  avoidCirculars?: Ref<boolean>
+  lazyRendering?: Ref<boolean>
+  defaultTag?: string
 }
 
 export interface UseThemeConfig {
