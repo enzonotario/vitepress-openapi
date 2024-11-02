30f550a7af3e12f6158f65d39d608834c0a1e499
Enzo Notario <enzo.notario@gmail.com>
2024-11-02 20:14:48 -0300

    fixes (#113)

--- src/components/ui/tabs/index.ts
+++ src/components/ui/tabs/index.ts
@@ -10,7 +10,6 @@ export const tabsTriggerVariants = cva(
   {
     variants: {
       variant: {
-        default: 'inline-flex items-center justify-center whitespace-nowrap rounded-sm px-3 py-1.5 text-sm font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:text-foreground',
         default: 'data-[state=active]:text-foreground',
         select: 'relative flex w-full',
         schemaTabs: 'data-[state=active]:bg-background data-[state=active]:text-foreground data-[state=active]:shadow-sm',
