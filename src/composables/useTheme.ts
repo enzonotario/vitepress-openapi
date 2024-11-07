cfc8b9c1e0625dcee0599616610b04b546b14436
enzonotario <enzo.notario@gmail.com>
2024-11-07 20:25:39 -0300

    chore: types

--- src/composables/useTheme.ts
+++ src/composables/useTheme.ts
@@ -1,7 +1,7 @@
 import { ref } from 'vue'
 import vitesseLight from 'shiki/themes/vitesse-light.mjs'
 import vitesseDark from 'shiki/themes/vitesse-dark.mjs'
-import type { IOARequest } from '../lib/codeSamples/request'
+import type { IOARequest, OARequest } from '../lib/codeSamples/request'
 import { generateCodeSample } from '../lib/codeSamples/generateCodeSample'
 import { deepUnref } from '../lib/deepUnref'
 import { locales } from '../locales'
@@ -101,8 +101,17 @@ export interface UseThemeConfig {
 export interface CodeSamplesConfig {
   langs: string[]
   defaultLang: string
-  generator: (lang: string, request: IOARequest) => string
+  availableLanguages: LanguageConfig[]
+  generator: GeneratorFunction
 }
+
+interface LanguageConfig {
+  lang: string
+  label: string
+  highlighter: string
+}
+
+type GeneratorFunction = (lang: string, request: OARequest) => string
 
 export const DEFAULT_OPERATION_SLOTS: OperationSlot[] = [
   'header',
@@ -118,7 +127,7 @@ export const DEFAULT_OPERATION_SLOTS: OperationSlot[] = [
   'footer',
 ]
 
-const availableLanguages = [
+const availableLanguages: LanguageConfig[] = [
   {
     lang: 'curl',
     label: 'cURL',
