21a6a27db7a0a9b53123ed67ea9f24714343d4cd
enzonotario <enzo.notario@gmail.com>
2024-11-28 19:45:49 -0300

    chore(getSchemaUiContentType): use json as fallback

--- src/lib/getSchemaUiContentType.ts
+++ src/lib/getSchemaUiContentType.ts
@@ -3,12 +3,9 @@ import { getSchemaUiJson } from './getSchemaUiJson'
 import type { OAProperty } from './getSchemaUi'
 
 export function getSchemaUiContentType(contentType: string, uiProperties: OAProperty[] | OAProperty, useExample = false): any {
-  switch (contentType) {
-    case 'application/xml':
-      return getSchemaUiXml(uiProperties, useExample)
-    case 'application/json':
-    default: {
-      return getSchemaUiJson(uiProperties, useExample)
-    }
+  if (contentType.includes('xml')) {
+    return getSchemaUiXml(uiProperties, useExample)
   }
+
+  return getSchemaUiJson(uiProperties, useExample)
 }
