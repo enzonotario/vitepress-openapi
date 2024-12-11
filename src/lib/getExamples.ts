f93ca99c5a16f9d4fb186828dead3d0f34bb987a
enzonotario <enzo.notario@gmail.com>
2024-12-10 23:39:56 -0300

    feat(parameters): show multiple examples

--- src/lib/getExamples.ts
+++ src/lib/getExamples.ts
@@ -1,11 +1,27 @@
 export function getExamples(property: any): any {
-  if (property?.example !== undefined) {
-    return [property.example]
-  }
-
-  if (property?.examples !== undefined) {
+  if (property?.examples && property?.examples?.length > 0) {
     return property.examples
   }
 
-  return undefined
+  if (property?.schema?.examples && property?.schema?.examples?.length > 0) {
+    return property.schema.examples
+  }
+
+  if (property?.example !== undefined) {
+    return property.example
+  }
+
+  if (property?.subexamples && property?.subexamples?.length > 0) {
+    return property.subexamples
+  }
+
+  if (property?.schema?.example !== undefined) {
+    return [property.schema.example]
+  }
+
+  if (property?.subexample !== undefined) {
+    return [property.subexample]
+  }
+
+  return null
 }
