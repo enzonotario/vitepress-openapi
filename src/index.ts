b2d9cb6071219bb480a34c03374318f584c4a3f4
enzonotario <enzo.notario@gmail.com>
2024-12-14 19:46:54 -0300

    chore(theme): optional openapi

--- src/index.ts
+++ src/index.ts
@@ -39,7 +39,7 @@ interface VPTheme {
 }
 
 export const theme = {
-  enhanceApp({ app, openapi }) {
+  enhanceApp({ app, openapi = null }) {
     if (openapi) {
       app.provide('openapi', openapi)
     }
