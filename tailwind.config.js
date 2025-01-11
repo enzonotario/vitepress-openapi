dd13a6145db18294e80282bccdf1c3c80d4461a4
Enzo Notario <enzo.notario@gmail.com>
2025-01-11 13:04:38 -0300

    fix(style): container size (#151)

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,13 +17,6 @@ module.exports = {
       : []),
   ],
   theme: {
-    container: {
-      center: true,
-      padding: '2rem',
-      screens: {
-        '2xl': '1400px',
-      },
-    },
     extend: {
       colors: {
         gray: colors.zinc,
