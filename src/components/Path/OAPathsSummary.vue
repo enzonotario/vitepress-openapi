3de287fff4198dd497bba8dafb60a0f3bb719add
enzonotario <enzo.notario@gmail.com>
2025-01-24 00:02:43 -0300

    style(OAPathsSummary): add bg and borders

--- src/components/Path/OAPathsSummary.vue
+++ src/components/Path/OAPathsSummary.vue
@@ -14,35 +14,39 @@ const emit = defineEmits([
 </script>
 
 <template>
-  <div class="">
-    <div class="grid grid-cols-[4rem,1fr] gap-2 overflow-auto">
-      <span />
-      <OAHeading level="h3" class="!mt-0">
+  <div class="bg-muted rounded border divide-y divide-[var(--vp-c-divider)]">
+    <div class="p-2 pl-4">
+      <OAHeading level="h3" class="!mt-0 !text-sm">
         {{ $t('Operations') }}
       </OAHeading>
     </div>
 
-    <template
-      v-for="(path, idx) in Object.keys(paths)"
-      :key="idx"
-    >
-      <a
-        v-for="method in Object.keys(paths[path])"
-        :key="`${path}-${method}`"
-        class="grid grid-cols-[4rem,1fr] items-center gap-2 !text-foreground !no-underline cursor-pointer"
-        @click="emit('pathClick', `#${paths[path][method].operationId}`)"
+    <div class="py-2">
+      <template
+        v-for="(path, idx) in Object.keys(paths)"
+        :key="idx"
       >
-        <span :class="[`text-method-${method}`]" class="text-sm text-right">
-          {{ method.toUpperCase() }}
-        </span>
-        <span
-          :class="{
-            'line-through': paths[path][method].deprecated,
-          }"
+        <a
+          v-for="method in Object.keys(paths[path])"
+          :key="`${path}-${method}`"
+          class="grid grid-cols-[4rem,1fr] items-center gap-2 !text-foreground !no-underline cursor-pointer"
+          :aria-label="`${method.toUpperCase()} ${path}`"
+          :title="`${method.toUpperCase()} ${path}`"
+          @click="emit('pathClick', `#${paths[path][method].operationId}`)"
         >
-          {{ path }}
-        </span>
-      </a>
-    </template>
+          <span :class="[`text-method-${method}`]" class="text-sm text-right">
+            {{ method.toUpperCase() }}
+          </span>
+          <span
+            :class="{
+              'line-through': paths[path][method].deprecated,
+            }"
+            class="truncate hover:underline"
+          >
+            {{ path }}
+          </span>
+        </a>
+      </template>
+    </div>
   </div>
 </template>
