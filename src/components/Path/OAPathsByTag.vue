1e0a578d13a73ebd27a0cdab6b4f0d0d56c7f844
enzonotario <enzo.notario@gmail.com>
2025-01-23 23:11:26 -0300

    chore(OAPathsByTags): split component into OAPathsByTag

---
+++ src/components/Path/OAPathsByTag.vue
@@ -0,0 +1,123 @@
+<script setup lang="ts">
+import { computed, defineProps, nextTick } from 'vue'
+import { getOpenApiInstance } from '../../lib/getOpenApiInstance'
+import { useTheme } from '../../composables/useTheme'
+import OAPathsSummary from '../Path/OAPathsSummary.vue'
+import { Button } from '../ui/button'
+import { Collapsible, CollapsibleTrigger } from '../ui/collapsible'
+import type { OperationSlot } from '../../types'
+import { scrollIntoOperationByOperationId } from '../../lib/utils'
+
+export interface Tag {
+  tag: string
+  paths: Record<string, any>
+  isOpen: boolean
+  description?: string
+}
+
+const props = defineProps({
+  openapi: {
+    type: Object,
+    required: true,
+  },
+  tag: {
+    type: Object as () => Tag,
+    required: true,
+  },
+  hidePathsSummary: {
+    type: Boolean,
+    default: undefined,
+  },
+})
+
+const slots = defineSlots<Record<string, OperationSlot>>()
+
+const themeConfig = useTheme()
+
+const openapi = props.openapi ?? getOpenApiInstance()
+
+const lazyRendering = themeConfig.getSpecConfig()?.lazyRendering?.value
+
+const showPathsSummary = computed(() => props.hidePathsSummary === undefined
+  ? themeConfig.getSpecConfig()?.showPathsSummary?.value
+  : !props.hidePathsSummary,
+)
+
+const hasDescription = computed(() => props.tag?.description?.trim() !== '')
+
+function onPathClick(tagPaths: { tag: string, paths: Record<string, any>, isOpen: boolean }, hash: string) {
+  tagPaths.isOpen = true
+
+  nextTick(() => {
+    scrollIntoOperationByOperationId({
+      hash,
+    })
+  })
+}
+</script>
+
+<template>
+  <Collapsible
+    v-model:open="props.tag.isOpen"
+  >
+    <OAHeading level="h1">
+      {{ props.tag.tag }}
+    </OAHeading>
+
+    <div
+      :class="{ 'md:grid-cols-2': showPathsSummary && hasDescription }"
+      class="grid grid-cols-1 gap-10"
+    >
+      <div v-if="hasDescription">
+        <p>
+          {{ props.tag.description }}
+        </p>
+      </div>
+
+      <div
+        v-if="showPathsSummary"
+        class="flex-1 my-[16px]"
+      >
+        <OAPathsSummary
+          :paths="props.tag.paths"
+          @path-click="onPathClick(props.tag, $event)"
+        />
+      </div>
+    </div>
+
+    <div
+      v-if="showPathsSummary || themeConfig.getSpecConfig()?.collapsePaths?.value === true"
+      class="flex justify-center"
+    >
+      <CollapsibleTrigger>
+        <Button>
+          {{ props.tag.isOpen ? $t('Hide operations') : $t('Show operations') }}
+        </Button>
+      </CollapsibleTrigger>
+    </div>
+
+    <hr>
+
+    <div
+      v-if="!lazyRendering || props.tag.isOpen"
+      class="flex flex-col space-y-10"
+      :class="[{ hidden: !props.tag.isOpen }]"
+    >
+      <OAPaths
+        :openapi="openapi"
+        :paths="props.tag.paths"
+      >
+        <!-- Expose all slots upwards -->
+        <template
+          v-for="(_, name) in slots"
+          #[name]="slotProps"
+        >
+          <slot
+            :name="name"
+            v-bind="slotProps || {}"
+          />
+        </template>
+      </OAPaths>
+    </div>
+  </Collapsible>
+</template>
--- src/components/Path/OAPathsByTags.vue
+++ src/components/Path/OAPathsByTags.vue
@@ -1,15 +1,12 @@
 <script setup lang="ts">
-import { computed, defineProps, nextTick, ref } from 'vue'
+import { defineProps, ref } from 'vue'
 import { useI18n } from 'vue-i18n'
 import type { OpenAPIV3 } from '@scalar/openapi-types'
 import { getOpenApiInstance } from '../../lib/getOpenApiInstance'
 import { useTheme } from '../../composables/useTheme'
 import OALazy from '../Common/Lazy/OALazy.vue'
-import OAPathsSummary from '../Path/OAPathsSummary.vue'
-import { Button } from '../ui/button'
-import { Collapsible, CollapsibleTrigger } from '../ui/collapsible'
 import type { OperationSlot } from '../../types'
-import { scrollIntoOperationByOperationId } from '../../lib/utils'
+import OAPathsByTag from './OAPathsByTag.vue'
 
 const props = defineProps({
   openapi: {
@@ -58,6 +55,7 @@ const internalTags = ref([
           tag: t(useTheme().getSpecConfig()?.defaultTag ?? 'Default'),
           paths: pathsWithoutTags,
           isOpen: !themeConfig.getSpecConfig()?.collapsePaths?.value,
+          description: '',
         },
       ]
     : []),
@@ -67,26 +65,12 @@ const internalTags = ref([
       tag: tag.tag,
       paths: tag.paths,
       isOpen: !themeConfig.getSpecConfig()?.collapsePaths?.value,
+      description: tagsInfo.find(tagInfo => tagInfo.name === tag.tag)?.description,
     }
   }),
 ])
 
-function onPathClick(tagPaths: { tag: string, paths: Record<string, any>, isOpen: boolean }, hash: string) {
-  tagPaths.isOpen = true
-
-  nextTick(() => {
-    scrollIntoOperationByOperationId({
-      hash,
-    })
-  })
-}
-
 const lazyRendering = themeConfig.getSpecConfig()?.lazyRendering?.value
-
-const showPathsSummary = computed(() => props.hidePathsSummary === undefined
-  ? themeConfig.getSpecConfig()?.showPathsSummary?.value
-  : !props.hidePathsSummary,
-)
 </script>
 
 <template>
@@ -95,70 +79,22 @@ const showPathsSummary = computed(() => props.hidePathsSummary === undefined
     :key="tagPaths.tag"
     :is-lazy="lazyRendering && tagIdx > 0"
   >
-    <Collapsible
+    <OAPathsByTag
       v-if="Object.keys(tagPaths.paths).length"
-      v-model:open="tagPaths.isOpen"
+      :openapi="openapi"
+      :tag="tagPaths"
+      :hide-paths-summary="props.hidePathsSummary"
     >
-      <OAHeading level="h1">
-        {{ tagPaths.tag }}
-      </OAHeading>
-
-      <div
-        :class="{ 'md:grid-cols-2': showPathsSummary }"
-        class="grid grid-cols-1 gap-10"
+      <!-- Expose all slots upwards -->
+      <template
+        v-for="(_, name) in slots"
+        #[name]="slotProps"
       >
-        <div>
-          <p v-if="tagsInfo.find(tag => tag.name === tagPaths.tag)?.description">
-            {{ tagsInfo.find(tag => tag.name === tagPaths.tag)?.description }}
-          </p>
-        </div>
-
-        <div
-          v-if="showPathsSummary"
-          class="flex-1 my-[16px]"
-        >
-          <OAPathsSummary
-            :openapi="openapi"
-            :paths="tagPaths.paths"
-            @path-click="onPathClick(tagPaths, $event)"
-          />
-        </div>
-      </div>
-
-      <div
-        v-if="showPathsSummary || themeConfig.getSpecConfig()?.collapsePaths?.value === true"
-        class="flex justify-center"
-      >
-        <CollapsibleTrigger>
-          <Button>
-            {{ tagPaths.isOpen ? $t('Hide operations') : $t('Show operations') }}
-          </Button>
-        </CollapsibleTrigger>
-      </div>
-
-      <hr>
-
-      <div
-        v-if="!lazyRendering || tagPaths.isOpen"
-        class="flex flex-col space-y-10"
-        :class="[{ hidden: !tagPaths.isOpen }]"
-      >
-        <OAPaths
-          :openapi="openapi"
-          :paths="tagPaths.paths"
-        >
-          <!-- Expose all slots upwards -->
-          <template
-            v-for="(_, name) in slots"
-            #[name]="slotProps"
-          >
-            <slot
-              :name="name"
-              v-bind="slotProps || {}"
-            />
-          </template>
-        </OAPaths>
-      </div>
-    </Collapsible>
+        <slot
+          :name="name"
+          v-bind="slotProps || {}"
+        />
+      </template>
+    </OAPathsByTag>
   </OALazy>
 </template>
