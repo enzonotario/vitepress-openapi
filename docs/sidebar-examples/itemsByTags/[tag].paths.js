68109c3950854179b6e0111ac7795a447cfc4ddd
enzonotario <enzo.notario@gmail.com>
2025-02-06 22:27:28 -0300

    docs(sidebar-examples): add pages

---
+++ docs/sidebar-examples/itemsByTags/[tag].paths.js
@@ -0,0 +1,17 @@
+import { usePaths } from 'vitepress-openapi'
+import spec from '../../public/openapi.json' with { type: 'json' }
+
+export default {
+    paths() {
+        return usePaths({ spec })
+            .getTags()
+            .map(({ name }) => {
+                return {
+                    params: {
+                        tag: name,
+                        pageTitle: `${name} - vitepress-openapi`,
+                    },
+                }
+            })
+    },
+}
