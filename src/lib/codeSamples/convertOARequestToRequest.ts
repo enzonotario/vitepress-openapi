c0e55bd47d32b8ce21ed151bf39478bef4af35a3
enzonotario <enzo.notario@gmail.com>
2025-02-16 19:24:15 -0300

    chore(request): http verbs with body

--- src/lib/codeSamples/convertOARequestToRequest.ts
+++ src/lib/codeSamples/convertOARequestToRequest.ts
@@ -6,7 +6,16 @@ export function OARequestToRequest(oaRequest: OARequest): Request {
   const query = new URLSearchParams(oaRequest.query).toString()
 
   const headers = new Headers(oaRequest.headers)
-  const body = ['POST', 'PUT', 'PATCH'].includes(method) && oaRequest.body ? JSON.stringify(oaRequest.body) : undefined
+  const body = [
+    'POST',
+    'PUT',
+    'PATCH',
+    'DELETE',
+    'OPTIONS',
+    'TRACE',
+  ].includes(method) && oaRequest.body
+    ? JSON.stringify(oaRequest.body)
+    : undefined
 
   if (body && !headers.has('content-type')) {
     headers.set('content-type', 'application/json')
