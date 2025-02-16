586d64d542ac79eaf167f20ebda25ef01b44e5d2
enzonotario <enzo.notario@gmail.com>
2025-02-16 19:18:26 -0300

    chore(processOpenAPI): async test

--- test/lib/processOpenAPI.test.ts
+++ test/lib/processOpenAPI.test.ts
@@ -1,11 +1,13 @@
 import { describe, expect, it } from 'vitest'
 import realSpec from '../../docs/public/openapi.json'
+import { processAsyncOpenAPI } from '../../src/lib/processOpenAPI/processAsyncOpenAPI'
 import { processOpenAPI } from '../../src/lib/processOpenAPI/processOpenAPI'
 
 describe('processOpenAPI', () => {
   it('processes the OpenAPI spec', async () => {
-    const parsedSPec = await processOpenAPI(realSpec)
+    let openapi = processOpenAPI(realSpec)
+    openapi = await processAsyncOpenAPI(openapi)
 
-    expect(parsedSPec).toMatchSnapshot()
+    expect(openapi).toMatchSnapshot()
   })
 })
