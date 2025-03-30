cf5fb4d62d60a3d736564e76e5399f4ea8752ae0
enzonotario <enzo.notario@gmail.com>
2025-03-30 19:41:52 -0300

    refactor: migrate from vue-i18n to @byjohann/vue-i18n

--- src/client.ts
+++ src/client.ts
@@ -2,8 +2,8 @@ import type { Awaitable } from 'vitepress'
 import type { EnhanceAppContext, Theme } from 'vitepress/client'
 import type { Component } from 'vue'
 import type { OpenApi } from './lib/OpenApi'
+import { createI18n } from '@byjohann/vue-i18n'
 import { watch } from 'vue'
-import * as VueI18n from 'vue-i18n'
 import * as components from './components'
 import { useShiki } from './composables/useShiki'
 import { DEFAULT_OPERATION_SLOTS, useTheme } from './composables/useTheme'
@@ -23,20 +23,18 @@ export const theme = {
     const themeConfig = useTheme()
 
     const i18nConfig = themeConfig.getI18nConfig()
-    const i18n: any = VueI18n.createI18n({
-      legacy: false,
-      locale: i18nConfig.locale.value,
-      fallbackLocale: i18nConfig.fallbackLocale.value,
+
+    const i18n = createI18n({
+      defaultLocale: i18nConfig.locale.value,
       messages: i18nConfig.messages,
     })
+
     app.use(i18n)
 
     watch(
       () => themeConfig.getLocale(),
       () => {
-        if (i18n.global.locale.value !== themeConfig.getLocale()) {
-          i18n.global.locale.value = themeConfig.getLocale()
-        }
+        i18n.setLocale(themeConfig.getLocale())
       },
       {
         immediate: true,
