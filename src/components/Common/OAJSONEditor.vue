d8ed8acc4878f794e4f35a8ed69d16c24a4b24d2
enzonotario <enzo.notario@gmail.com>
2025-04-29 23:24:13 -0300

    fix(OAJSONEditor): parse JSON value before emitting update

--- src/components/Common/OAJSONEditor.vue
+++ src/components/Common/OAJSONEditor.vue
@@ -18,7 +18,7 @@ const emit = defineEmits(['update:modelValue'])
 
 const value = computed({
   get: () => props.modelValue,
-  set: val => emit('update:modelValue', val),
+  set: val => emit('update:modelValue', JSON.parse(val)),
 })
 
 const themeConfig = useTheme()
