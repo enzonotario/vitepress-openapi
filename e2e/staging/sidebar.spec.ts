fe52f6502e324ee7d58c7952e030b89baa003676
enzonotario <enzo.notario@gmail.com>
2025-06-15 21:53:14 -0300

    chore(e2e): add tests for sidebar examples

---
+++ e2e/staging/sidebar.spec.ts
@@ -0,0 +1,16 @@
+import { expect, test } from '@playwright/test'
+import { examples } from '../../docs/sidebars.ts'
+
+const exampleSlugs = examples.map(example => example.slug)
+
+for (const exampleSlug of exampleSlugs) {
+  test(`sidebar example: ${exampleSlug}`, async ({ page }) => {
+    await page.goto(`/sidebar-examples/${exampleSlug}`)
+
+    await page.waitForSelector('h1')
+
+    await expect(page).toHaveScreenshot({
+      fullPage: true,
+    })
+  })
+}
