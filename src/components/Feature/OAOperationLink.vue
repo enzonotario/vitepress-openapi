c4e966681ec9b218d8a374003cff54ccac26af52
enzonotario <enzo.notario@gmail.com>
2025-06-23 22:17:37 -0300

    style(OAOperationLink): adjust style to match default vitepress

--- src/components/Feature/OAOperationLink.vue
+++ src/components/Feature/OAOperationLink.vue
@@ -23,23 +23,19 @@ withDefaults(defineProps<Props>(), {
     <span class="OAOperationLink-badge" :class="[`OAMethodBadge--${method.toLowerCase()}`]">
       {{ method.toUpperCase() }}
     </span>
-    <span>{{ title }}</span>
+    <span class="OAOperationLink-title">{{ title }}</span>
   </component>
 </template>
 
 <style>
 .OAOperationLink {
   @apply inline-block px-1 space-x-1 rounded cursor-pointer;
-  text-decoration: none !important;
-  color: var(--vp-c-text-1) !important;
-  font-weight: normal !important;
-}
-
-.OAOperationLink:hover {
-  color: var(--vp-c-brand) !important;
 }
 
 .OAOperationLink-badge {
   @apply px-1 py-0.5 text-xs rounded;
+  font-weight: normal !important;
+  text-decoration: none !important;
+  display: inline-block;
 }
 </style>
