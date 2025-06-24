d532f8e9dd1115d2b1207ab1e666747ff9709fab
enzonotario <enzo.notario@gmail.com>
2025-06-24 18:56:26 -0300

    feat(OAOperationLink): add support for default slot with Markdown rendering

--- src/components/Feature/OAOperationLink.vue
+++ src/components/Feature/OAOperationLink.vue
@@ -1,29 +1,55 @@
 <script setup lang="ts">
 import type { OpenAPIV3 } from '@scalar/openapi-types'
+import type { Slots } from 'vue'
+import { computed, useSlots } from 'vue'
+import OAMarkdown from '../Common/OAMarkdown.vue'
 
 interface Props {
   href?: string
   method?: OpenAPIV3.HttpMethods | string
   title?: string
+  operationId?: string
 }
 
-withDefaults(defineProps<Props>(), {
+const props = withDefaults(defineProps<Props>(), {
   href: undefined,
   method: '',
   title: '',
+  operationId: undefined,
+})
+
+const slots: Slots = useSlots()
+
+function hasSlot(name: string): boolean {
+  return !!slots[name]
+}
+
+const slotContent = computed<string>(() => {
+  if (!hasSlot('default')) {
+    return ''
+  }
+
+  const slot = slots.default?.()
+
+  if (!slot || !Array.isArray(slot) || slot.length === 0) {
+    return ''
+  }
+
+  return typeof slot[0].children === 'string' ? slot[0].children : ''
 })
 </script>
 
 <template>
   <component
-    :is="href ? 'a' : 'span'"
-    :href="href"
+    :is="props.href ? 'a' : 'span'"
+    :href="props.href"
     class="OAOperationLink group/oaOperationLink"
   >
     <span class="OAOperationLink-badge" :class="[`OAMethodBadge--${method.toLowerCase()}`]">
       {{ method.toUpperCase() }}
     </span>
-    <span class="OAOperationLink-title">{{ title }}</span>
+    <OAMarkdown v-if="hasSlot('default')" :content="slotContent" class="inline-block" />
+    <span v-else class="OAOperationLink-title">{{ props.title }}</span>
   </component>
 </template>
 
