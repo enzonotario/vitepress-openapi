b5aff25a4b6bb2bcde87f0237cea6349e05c1078
enzonotario <enzo.notario@gmail.com>
2026-03-15 18:39:44 -0300

    feat(exports): export components for footer, header badges, code samples, and security

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,6 @@
 import OAVueJsonPretty from './Common/Highlight/OAVueJsonPretty.vue'
+import OAFooter from './Common/OAFooter.vue'
+import OAHeaderBadges from './Common/OAHeaderBadges.vue'
 import OAHeading from './Common/OAHeading.vue'
 import OAMarkdown from './Common/OAMarkdown.vue'
 import OAInfo from './Feature/OAInfo.vue'
@@ -10,14 +12,20 @@ import OAOperationLink from './Feature/OAOperationLink.vue'
 import OAServers from './Feature/OAServers.vue'
 import OASpec from './Feature/OASpec.vue'
 import OASpecSkeleton from './Feature/OASpecSkeleton.vue'
+import OAOperationTags from './Operation/OAOperationTags.vue'
 import OAParameter from './Parameter/OAParameter.vue'
 import OAParameters from './Parameter/OAParameters.vue'
 import OAPathEndpoint from './Path/OAPathEndpoint.vue'
 import OAPlayground from './Playground/OAPlayground.vue'
 import OARequestBody from './Request/OARequestBody.vue'
 import OAResponses from './Response/OAResponses.vue'
+import OACodeSamples from './Sample/OACodeSamples.vue'
+import OASecurity from './Security/OASecurity.vue'
 
 export {
+  OACodeSamples,
+  OAFooter,
+  OAHeaderBadges,
   OAHeading,
   OAInfo,
   OAIntroduction,
@@ -26,12 +34,14 @@ export {
   OAOperation,
   OAOperationContext,
   OAOperationLink,
+  OAOperationTags,
   OAParameter,
   OAParameters,
   OAPathEndpoint,
   OAPlayground,
   OARequestBody,
   OAResponses,
+  OASecurity,
   OAServers,
   OASpec,
   OASpecSkeleton,
