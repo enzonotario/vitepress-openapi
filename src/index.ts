33bf0d7123a8a572f1f3271bba4cc7b163517b81
enzonotario <enzo.notario@gmail.com>
2026-03-19 20:53:54 -0300

    chore: format

--- src/index.ts
+++ src/index.ts
@@ -8,8 +8,8 @@ export {
 } from './composables/useOpenapi'
 export { usePaths } from './composables/usePaths'
 export { useSidebar } from './composables/useSidebar'
-export { minifyHtml } from './lib/utils/minifyHtml'
 export { createOpenApiSpec, type OpenApiSpecInstance } from './lib/spec/createOpenApiSpec'
+export { minifyHtml } from './lib/utils/minifyHtml'
 export { parseSpec } from './lib/utils/parseSpec'
 
 export const httpVerbs: readonly OpenAPIV3.HttpMethods[] = ['get', 'put', 'post', 'delete', 'options', 'head', 'patch', 'trace'] as const
