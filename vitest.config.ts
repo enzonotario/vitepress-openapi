adc54e97990f45165fc04be4a007e7a36c7dde4d
enzonotario <enzo.notario@gmail.com>
2026-07-05 17:05:02 -0300

    chore(tests): add path aliasing in vitest configuration for improved module resolution

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,8 +1,16 @@
+import { resolve } from 'node:path'
 import vue from '@vitejs/plugin-vue'
 import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+      'vitepress-openapi/client': resolve(__dirname, 'src/client.ts'),
+      'vitepress-openapi': resolve(__dirname, 'src/index.ts'),
+    },
+  },
   test: {
     include: ['test/**/*.test.{ts,js}'],
   },
